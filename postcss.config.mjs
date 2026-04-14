6210afb7319da69ee13d2d47914e541bbb8757e2
Vaasu Gagneja <v79629550@gmail.com>
2026-04-14 16:19:40 +0530

    Add files via upload

---
+++ postcss.config.mjs
@@ -0,0 +1,10 @@
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};
+
+export default config;
+//https://www.youtube.com/watch?v=RtWwNWxIDzE
+//https://www.youtube.com/shorts/GV7yyNpSO-4
+//https://www.youtube.com/shorts/-NrToZG13DI
